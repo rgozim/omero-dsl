28ae431364f443ed5cf74df3bddf7410472d4641
Riad Gozim <rgozim1@gmail.com>
2018-07-10 16:58:26 +0100

    Numerous changes Removed unused tests Updated gradle wrapper Set version to 0.1.0 Updated readme

---
+++ dslplugin/src/test/groovy/DslExtensionTest.groovy
@@ -0,0 +1,71 @@
+import extensions.DslExtension
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import spock.lang.Specification
+
+class DslExtensionTest extends Specification {
+
+    @Rule
+    final TemporaryFolder testProjectDir = new TemporaryFolder()
+    Project project
+
+    def setup() {
+        project = ProjectBuilder.builder().build()
+    }
+
+    def "OutputPath is absolute"() {
+        when:
+        def dsl = new DslExtension()
+        dsl.outputPath "someFolder"
+
+        then:
+        dsl.outputPath.isAbsolute()
+    }
+
+    def "Templates support multiple dirs"() {
+        given:
+        def folderA = createFilesInFolder(testProjectDir.newFolder("A"))
+        def folderB = createFilesInFolder(testProjectDir.newFolder("B"))
+
+        when:
+        def dsl = new DslExtension()
+        dsl.templateFiles project.fileTree(dir: folderA, include: '*.file')
+        dsl.templateFiles project.fileTree(dir: folderB, include: '*.file')
+
+        then:
+        dsl.templateDir.size() == 6
+    }
+
+    def "OmeXmlFiles support multiple dirs"() {
+        given:
+        def folderA = createFilesInFolder(testProjectDir.newFolder("A"))
+        def folderB = createFilesInFolder(testProjectDir.newFolder("B"))
+
+        when:
+        def dsl = new DslExtension()
+        dsl.mappingFiles project.fileTree(dir: folderA, include: '*.file')
+        dsl.mappingFiles project.fileTree(dir: folderB, include: '*.file')
+
+        then:
+        dsl.mappingFiles.size() == 6
+    }
+
+    // Create fake ome.xml files
+    def createFilesInFolder(File folder) {
+        createFile(folder, "fileA.file")
+        createFile(folder, "fileB.file")
+        createFile(folder, "fileC.file")
+        return folder
+    }
+
+    def createFile(File folder, String fileName) {
+        def file = new File(folder, fileName)
+        if (!file.createNewFile()) {
+            throw new IOException("File already exists")
+        }
+        return file
+    }
+
+}
