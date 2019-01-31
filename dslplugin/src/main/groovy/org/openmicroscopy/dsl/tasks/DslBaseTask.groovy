5ad15c5a7bd2eba8fe4df530955ec3fdc63548ca
Riad Gozim <rgozim1@gmail.com>
2019-01-31 22:44:30 +0000

    Fixed dslbasetask bug

--- dslplugin/src/main/groovy/org/openmicroscopy/dsl/tasks/DslBaseTask.groovy
+++ dslplugin/src/main/groovy/org/openmicroscopy/dsl/tasks/DslBaseTask.groovy
@@ -5,8 +5,6 @@ import ome.dsl.velocity.Generator
 import org.apache.velocity.app.VelocityEngine
 import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
-import org.gradle.api.Task
-import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.FileCollection
 import org.gradle.api.logging.Logger
 import org.gradle.api.logging.Logging
@@ -15,9 +13,9 @@ import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.TaskAction
-import org.gradle.api.tasks.TaskProvider
 import org.gradle.api.tasks.util.PatternSet
 import org.openmicroscopy.dsl.FileTypes
+
 
 @CompileStatic
 abstract class DslBaseTask extends DefaultTask {
@@ -43,73 +41,35 @@ abstract class DslBaseTask extends DefaultTask {
 
     @TaskAction
     void apply() {
-        omeXmlFiles.asFileTree.files.each {
-            Log.info("OMEXML $it")
-        }
+        VelocityEngine ve = new VelocityEngine(velocityProperties)
 
-        databaseTypes.asFileTree.files.each {
-            Log.info("DBTYPEFILE $it")
-        }
-
-//        VelocityEngine ve = new VelocityEngine(velocityProperties)
-
-//        // Build our file generator
-//        def builder = createGenerator()
-//        builder.velocityEngine = ve
-//        builder.profile = databaseType
-//        builder.template = template
-//        builder.databaseTypes = _getDatabaseTypeProperties()
-//        builder.omeXmlFiles = _getOmeXmlFiles()
-//        builder.build().call()
+        // Build our file generator
+        def builder = createGenerator()
+        builder.velocityEngine = ve
+        builder.profile = databaseType
+        builder.template = template
+        builder.databaseTypes = _getDatabaseTypeProperties()
+        builder.omeXmlFiles = _getOmeXmlFiles()
+        builder.build().call()
     }
 
     abstract protected Generator.Builder createGenerator()
 
-    //
-    // omeXmlFiles
-    //
+    void omeXmlFiles(Object... files) {
+        setOmeXmlFiles(files)
+    }
 
-//    void omeXmlFiles(Iterable<?> files) {
-//        setOmeXmlFiles(files)
-//    }
-//
-//    void omeXmlFiles(Object... files) {
-//        setOmeXmlFiles(files)
-//    }
-//
-//    void setOmeXmlFiles(Iterable<?> files) {
-//        this.omeXmlFiles.setFrom(files) //= project.files(files)
-//    }
-//
-//    void setOmeXmlFiles(Object... files) {
-//        this.omeXmlFiles.setFrom(files)
-//    }
+    void setOmeXmlFiles(Object... files) {
+        this.omeXmlFiles = project.files(files)
+    }
 
-    //
-    // databaseTypes
-    //
+    void databaseTypes(Object... files) {
+        setDatabaseTypes(files)
+    }
 
-
-//    void databaseTypes(Iterable<?> files) {
-//        setDatabaseTypes(files)
-//    }
-//
-//    void databaseTypes(Object... files) {
-//        setDatabaseTypes(files)
-//    }
-//
-//    void setDatabaseTypes(Iterable<?> files) {
-//        this.databaseTypes.setFrom(files)
-//    }
-//
-//    void setDatabaseTypes(Object... files) {
-//        this.databaseTypes.setFrom(files)
-//    }
-
-    //
-    // template
-    //
-
+    void setDatabaseTypes(Object... files) {
+        this.databaseTypes = project.files(files)
+    }
 
     void template(Object dir) {
         setTemplate(dir)
@@ -118,10 +78,6 @@ abstract class DslBaseTask extends DefaultTask {
     void setTemplate(Object dir) {
         this.template = project.file(dir)
     }
-
-    //
-    // databaseType
-    //
 
     void databaseType(String type) {
         setDatabaseType(type)
@@ -142,28 +98,18 @@ abstract class DslBaseTask extends DefaultTask {
     }
 
     private File _getDatabaseTypesFile() {
-        def dbtypefiles = _getFilesInCollection(databaseTypes, "properties")
-        return dbtypefiles.find {
-            it.name == "${databaseType}-type.${FileTypes.EXTENSION_DB_TYPE}"
+        return _getFilesInCollection(databaseTypes, FileTypes.PATTERN_DB_TYPE).find { File file ->
+            file.name.contains(databaseType)
         }
     }
 
     private Collection<File> _getOmeXmlFiles() {
-        return _getFilesInCollection(omeXmlFiles, FileTypes.EXTENSION_OME_XML)
+        return _getFilesInCollection(omeXmlFiles, FileTypes.PATTERN_OME_XML)
     }
 
-    private static Collection<File> _getFilesInCollection(FileCollection collection, String extension) {
-        def directories = collection.findAll { File file ->
-            file.isDirectory()
-        }
-
-        def files = collection.findAll { File file ->
-            file.isFile() && file.name.matches("$extension")
-        }
-
-        return files + directories.collectMany {
-            project.fileTree(dir: it, include: "**/*.$extension").files
-        }
+    private static Collection<File> _getFilesInCollection(FileCollection collection, String include) {
+        PatternSet patternSet = new PatternSet().include(include)
+        return collection.asFileTree.matching(patternSet).files
     }
 
 }
